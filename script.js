f90cdd3138a4764dd96b1af22c5991e830dd921e
William Gulian <williamgulian@gmail.com>
2015-06-10 19:19:49 -0400

    Added SideScroll4

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 /*jshint browser:true */
 /*global $:false, jQuery:false, console:false, escape:false */
 
-var d;
-
 $(function () {
-    d = this;
+
 
     function addApp(title, img, body, url) {
         var html = $("#template-app-entry").html();
@@ -21,7 +19,12 @@ $(function () {
 
     }
 
+    //Fill in content
 
-    addApp("ImageStorm", "assets/img_storm.png", "Rain images from the sky!", "ImageStorm/");
+    addApp("ImageStorm <span class=\"label label-default\">New</span>", "assets/img_storm.png", "Rain images from the sky!", "ImageStorm/");
+    addApp("Side Scroll 4 <span class=\"label label-default\">New</span>", "assets/side_scroll_4.png", "A 2D Side Scroller!", "Side-Scroll-4/");
+
+    //Do Bootstrap stuff
+    $('[data-toggle="tooltip"]').tooltip();
 
 });
