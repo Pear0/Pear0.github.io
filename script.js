e167bf93abf5b9621ddc586e9a144eba68d513a5
William Gulian <williamgulian@gmail.com>
2015-06-10 20:02:04 -0400

    added hash to ImageStorm

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ $(function () {
 
     //Fill in content
 
-    addApp("ImageStorm <span class=\"label label-default\">New</span>", "assets/img_storm.png", "Rain images from the sky!", "ImageStorm/");
+    addApp("ImageStorm <span class=\"label label-default\">New</span>", "assets/img_storm.png", "Rain images from the sky!", "ImageStorm/#cats");
     addApp("Side Scroll 4 <span class=\"label label-default\">New</span>", "assets/side_scroll_4.png", "A 2D Side Scroller!", "Side-Scroll-4/");
 
     //Do Bootstrap stuff
